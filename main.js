3708c99132cf790242ca57323907fd56f348424b
VrindaTyagi123 <83289238+VrindaTyagi123@users.noreply.github.com>
2021-11-03 17:06:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,43 +1,36 @@
-var canvas = new fabric.Canvas('myCanvas');
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
 
-ball_y = 0;
-ball_x = 0;
-hole_y = 400;
-hole_x = 800;
+greencar_width = 75;
+greencar_height = 100;
 
+background_image = "parkingLot.jpg";
+greencar_image = "car2.png";
 
-block_image_width = 5;
-block_image_height = 5;
+greencar_x = 5;
+greencar_y = 225;
 
-function load_img(){
-	fabric.Image.fromURL("golf-h.png", function(Img){
-		hole_obj = Img;
-		hole_obj.scaleToWidth(50);
-		hole_obj.scaleToHeight(50);
-		hole_obj.set({
-
-			top:hole_y,
-			left:hole_x
-		});
-		canvas.add(hole_obj);
-	});
-	new_image();
+function add() {
+	background_imgTag = new Image();
+	background_imgTag.onload = uploadBackground;
+	background_imgTag.src = background_image;
+	greencar_imgTag = new Image();
+	greencar_imgTag.onload = uploadgreencar;
+	greencar_imgTag.src = greencar_image;
 }
 
+function uploadBackground() {
+	
+	ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
 
-function new_image(){
-	fabric.Image.fromURL("ball.png", function(Img){
-		ball_obj = Img;
-		ball_obj.scaleToWidth(50);
-		ball_obj.scaleToHeight(50);
-		ball_obj.set({
-
-			top:hole_y,
-			left:hole_x
-		});
-		canvas.add(ball_obj);
-	});
 }
+
+function uploadgreencar() {
+
+	ctx.drawImage(greencar_imgTag, greencar_x, greencar_y, greencar_width, greencar_height);
+
+}
+
 
 window.addEventListener("keydown", my_keydown);
 
@@ -45,81 +38,73 @@ function my_keydown(e)
 {
 	keyPressed = e.keyCode;
 	console.log(keyPressed);
-	if((ball_x==hole_x)&&(ball_y==hole_y)){
-		canvas.remove(ball_obj);
-	}
-	document.getElementById("hd3").innerHTML = "You Have Hit The Goal!!!!";
-	document.getElementById("myCanvas").style.borderColor="red";
-	{
 		if(keyPressed == '38')
 		{
 			up();
 			console.log("up");
 		}
+	
 		if(keyPressed == '40')
 		{
 			down();
 			console.log("down");
 		}
+		
 		if(keyPressed == '37')
 		{
 			left();
 			console.log("left");
 		}
+	
 		if(keyPressed == '39')
 		{
 			right();
 			console.log("right");
 		}
-	}
-	
-	function up()
-	{
-		if(ball_y <=0)
-		 {
-			 ball_y = ball_y + block_image_height;
-			 console.log("block image height = " + block_image_height);
-			 console.log("When Up arrow key pressed, X = " + ball_x + " , Y = " + ball_y);
-			 canvas.remove(ball_obj);
-			 new_image();
-		 }
-	}
-
-	function down()
-	{
-		 if(ball_y <=450)
-		 {
-			 ball_y = ball_y + block_image_height;
-			 console.log("block image height = " + block_image_height);
-			 console.log("When down arrow key pressed, X = " + ball_x + " , Y = " + ball_y);
-			 canvas.remove(ball_obj);
-			 new_image();
-		 }
-	}
-
-	function left()
-	{
-		if(ball_x >5)
-		{
-			ball_x = ball_x + block_image_height;
-			 console.log("block image height = " + block_image_height);
-			 console.log("When Left arrow key pressed, X = " + ball_x + " , Y = " + ball_y);
-			 canvas.remove(ball_obj);
-			 new_image();
-		}
-	}
-
-	function right()
-	{
-		if(ball_x <=1050)
-		{
-			ball_x = ball_x + block_image_height;
-			 console.log("block image height = " + block_image_height);
-			 console.log("When Right arrow key pressed, X = " + ball_x + " , Y = " + ball_y);
-			 canvas.remove(ball_obj);
-			 new_image();
-		}
-	}
-	
+		
+		
 }
 
+function up()
+{
+	if(greencar_y>=0);
+	{
+		greencar_y = greencar_y-10;
+		console.log("When up arrow is pressed, X = " + greencar_x + " | y = "+greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
+}
+
+function down()
+{
+	if(greencar_y<=500);
+	{
+		greencar_y = greencar_y+10;
+		console.log("When down arrow is pressed, X = " + greencar_x + " | y = "+greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
+}
+
+function left()
+{
+	if(greencar_x>=0);
+	{
+		greencar_x = greencar_x-10;
+		console.log("When left arrow is pressed, X = " + greencar_x + " | y = "+greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
+}
+
+function right()
+{
+	if(greencar_x<=700);
+	{
+		greencar_x = greencar_x+10;
+		console.log("When right arrow is pressed, X = " + greencar_x + " | y = "+greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
+}
